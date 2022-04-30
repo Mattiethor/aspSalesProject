8688857a8240288fb284c266663a904c422dcc04
Matthías Enok Þórarinsson <met1@nemandi.ntv.is>
2022-04-30 08:20:23 +0000

    Added and SaleContext

---
+++ aspSaleApp/Data/SaleContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ntv_database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ntv_database.Migrations
+{
+    public class SaleContext : DbContext
+    {
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<ItemOrder> ItemsOrders { get; set; }
+
+ 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SaleDb");
+        }
+
+    }
+    }
+
+
---
+++ aspSaleApp/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ntv_database.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Sales = new List<Sale>();
+        }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        //move to class, change to int, add address, zip code.
+        public string Address { get; set; }
+
+        public List<Sale> Sales { get; set; }
+    }
+}
